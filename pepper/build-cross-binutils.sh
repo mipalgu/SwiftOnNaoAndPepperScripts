0d5d1892dafba3a4832cee3945150f49c8bdc158
Callum McColl <callum.mccoll1@gmail.com>
2019-08-04 03:26:52 +1000

    Enable multilib in binutils

--- pepper/build-cross-binutils.sh
+++ pepper/build-cross-binutils.sh
@@ -8,7 +8,7 @@ then
     rm -rf $WD/build-binutils
     mkdir $WD/build-binutils
     cd $WD/build-binutils
-    PATH="$CROSS_TOOLCHAIN_DIR/bin:$PATH" $WD/binutils-$BINUTILS_VERSION/configure --prefix="$CROSS_DIR" --target=${TRIPLE} --enable-gold=yes --with-sysroot="$LFS"
+    PATH="$CROSS_TOOLCHAIN_DIR/bin:$PATH" $WD/binutils-$BINUTILS_VERSION/configure --prefix="$CROSS_DIR" --target=${TRIPLE} --enable-gold=yes --enable-multilib=yes --with-sysroot="$LFS"
     PATH="$CROSS_TOOLCHAIN_DIR/bin:$PATH" make -j${PARALLEL}
     PATH="$CROSS_TOOLCHAIN_DIR/bin:$PATH" make install
     cd $WD
