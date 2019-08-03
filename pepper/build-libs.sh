80f4135c2ab0addd7f600eb74ae16d640590a73c
Callum McColl <callum.mccoll1@gmail.com>
2019-08-04 02:39:17 +1000

    Build icu - currently not working

--- pepper/build-libs.sh
+++ pepper/build-libs.sh
@@ -5,30 +5,28 @@ source setup.sh
 
 if [ ! -f $WD/build-icu-host/.build-icu-host ]
 then
-    cd $WD/apple/icu/icu4c/source
+    cd $WD/icu-$ICU4C_VERSION/source
     [ -f Makefile ] && make distclean
     rm -rf $WD/build-icu-host
     mkdir $WD/build-icu-host
     cd $WD/build-icu-host
     if [[ "$OSTYPE" == "darwin"* ]]
     then
-        CC="xcrun clang" CXX="xcrun clang++" $WD/apple/icu/icu4c/source/runConfigureICU MacOSX --prefix=$WD/icu
+        CC="xcrun clang" CXX="xcrun clang++" $WD/icu-$ICU4C_VERSION/source/runConfigureICU MacOSX --prefix=$WD/icu
     else
-        $WD/apple/icu/icu4c/source/runConfigureICU Linux
+        $WD/icu-$ICU4C_VERSION/source/runConfigureICU Linux
     fi
     make
     touch .build-icu-host
     cd $WD
 fi
 
-source cross.sh
-
-cd $WD/apple/icu/icu4c/source
+cd $WD/icu-$ICU4C_VERSION/source
 [ -f Makefile ] && make distclean
 rm -rf $WD/build-icu
 mkdir $WD/build-icu
 cd $WD/build-icu
-PATH="$CROSS_DIR/bin:$PATH" CFLAGS="$INCLUDE_FLAGS $BINARY_FLAGS" CXXFLAGS="$INCLUDE_FLAGS $BINARY_FLAGS" LDFLAGS="$LINK_FLAGS" $WD/apple/icu/icu4c/source/configure --prefix=$INSTALL_PREFIX --host=${TRIPLE} --with-cross-build="$WD/build-icu-host"
-PATH="$CROSS_DIR/bin:$PATH" make
-PATH="$CROSS_DIR/bin:$PATH" make install
+PATH="$CROSS_DIR/bin:$CROSS_DIR/$TRIPLE/bin:$PATH" CC="$CROSS_DIR/bin/$TRIPLE-gcc" CXX="$CROSS_DIR/bin/$TRIPLE-g++" AR="$CROSS_DIR/bin/$TRIPLE-ar" LD="$CROSS_DIR/bin/$TRIPLE-ld" $WD/icu-$ICU4C_VERSION/source/configure --prefix=$INSTALL_PREFIX --host=${TRIPLE} --with-cross-build="$WD/build-icu-host"
+make
+make install
 cd $WD
