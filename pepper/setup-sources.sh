1f3d1ca67e6cecff1ef49fcb82c2777cebd9811a
Callum McColl <callum.mccoll1@gmail.com>
2019-08-04 10:49:53 +1000

    Modified setup-sources to download gcc

--- pepper/setup-sources.sh
+++ pepper/setup-sources.sh
@@ -9,6 +9,17 @@ then
     tar -xzf binutils-$BINUTILS_VERSION.tar.gz
 fi
 
+if [ ! -d "gcc-$GCC_VERSION/cloog" ]
+then
+    if [ ! -d "gcc-$GCC_VERSION" ]
+    then
+        [ ! -f "gcc-${GCC_VERSION}.tar.gz" ] && wget https://ftp.gnu.org/gnu/gcc/gcc-${GCC_VERSION}/gcc-${GCC_VERSION}.tar.gz
+        tar -xzf gcc-${GCC_VERSION}.tar.gz
+    fi
+    cd gcc-$GCC_VERSION && ./contrib/download_prerequisites
+    cd $WD
+fi
+
 if [ ! -d "icu-$ICU4C_VERSION" ]
 then
     [ ! -f "icu4c-$ICU4C_VERSION.tar.gz" ] && wget http://download.icu-project.org/files/icu4c/$ICU4C_MAJOR_VERSION.$ICU4C_MINOR_VERSION/icu4c-$ICU4C_VERSION-src.tgz
