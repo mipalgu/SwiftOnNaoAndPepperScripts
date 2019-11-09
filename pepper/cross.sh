009d8b0268b28cbd75a3c2d1a37c8a21ae8e66ca
Callum McColl <callum.mccoll1@gmail.com>
2019-11-09 01:54:45 +0000

    Add the install prefix libs and include paths

--- pepper/cross.sh
+++ pepper/cross.sh
@@ -6,16 +6,21 @@ PREFIX_PATHS=`echo "$PREFIXES" | sed 's/\$\|:/\/bin:/g'`
 PREFIX_LIBRARY_PATHS=`echo "$PREFIXES" | sed 's/\$\|:/\/lib:/g'`
 PREFIX_INCLUDE_PATHS=`echo "$PREFIXES" | sed 's/\$\|:/\/include:/g'`
 
+SYSROOT_LIBRARY_PATH="$INSTALL_PREFIX/lib"
+SYSROOT_INCLUDE_PATH="$INSTALL_PREFIX/include"
+
 # Paths that cmake should use. 
 export PREFIX_PATH="$PREFIXES"
-export PATH="$SYSROOT_PATH:/usr/local/bin:/usr/bin:/bin"
+export PATH="/usr/local/bin:/usr/bin:/bin"
 export LIBRARY_PATH="$SYSROOT_LIBRARY_PATH:$LFS/usr/local/lib:$LFS/usr/lib:$LFS/lib:$LFS:$LFS/lib/gcc/${TRIPLE}/$GCC_VERSION"
 export LD_LIBRARY_PATH="$LIBRARY_PATH"
-export CPATH="$SYSROOT_CPATH"
+export CPATH="$SYSROOT_INCLUDE_PATH"
 export LANG=/usr/lib/locale/C.UTF-8
 
 # For each path in the toolchain, we create the -L -I and -B flags.
 export BINARY_FLAGS=`echo "$GCC_TOOLCHAIN" | sed 's/^/ -B/g' | sed 's/:/ -B/g'`
-export INCLUDE_FLAGS=`echo "$PREFIX_INCLUDE_PATHS" | sed 's/^/ -I/g' | sed 's/:/ -I/g'`
-L_FLAGS=`echo "$PREFIX_LIBRARY_PATHS:$GCC_TOOLCHAIN" | sed 's/^/ -L/g' | sed 's/:/ -L/g'`
+export INCLUDE_FLAGS=`echo "$PREFIX_INCLUDE_PATHS$SYSROOT_INCLUDE_PATH" | sed 's/^/ -I/g' | sed 's/:/ -I/g'`
+L_FLAGS=`echo "$PREFIX_LIBRARY_PATHS$SYSROOT_LIBRARY_PATH:$GCC_TOOLCHAIN" | sed 's/^/ -L/g' | sed 's/:/ -L/g'`
+echo "INCLUDE_FLAGS: $INCLUDE_FLAGS"
+echo "LFLAGS: $L_FLAGS"
 export LINK_FLAGS="-R$INSTALL_PREFIX/usr/local/lib -R$INSTALL_PREFIX/usr/lib -R$INSTALL_PREFIX/lib -R/usr/lib $L_FLAGS"
