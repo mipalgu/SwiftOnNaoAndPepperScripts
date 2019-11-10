e741ea5616c742b9f8e4e8c9bb809527a20a3477
Morgan McColl <morgan.mccoll@alumni.griffithuni.edu.au>
2019-11-10 04:58:17 +0000

    Copied all isntead.

--- pepper/finalise.sh
+++ pepper/finalise.sh
@@ -5,6 +5,4 @@ source setup.sh
 tar -czf pepper.tar.gz $INSTALL_PREFIX/lib
 rm -rf $CROSS_TOOLCHAIN_DIR/$TRIPLE/sysroot/home
 mkdir -p $CROSS_TOOLCHAIN_DIR/$TRIPLE/sysroot/home/nao/swift-tc
-cp -r $INSTALL_PREFIX/lib $CROSS_TOOLCHAIN_DIR/$TRIPLE/sysroot/home/nao/swift-tc/
-cp -r $INSTALL_PREFIX/local $CROSS_TOOLCHAIN_DIR/$TRIPLE/sysroot/home/nao/swift-tc/
-cp -r $INSTALL_PREFIX/include $CROSS_TOOLCHAIN_DIR/$TRIPLE/sysroot/home/nao/swift-tc/
+cp -r $INSTALL_PREFIX/* $CROSS_TOOLCHAIN_DIR/$TRIPLE/sysroot/home/nao/swift-tc/
