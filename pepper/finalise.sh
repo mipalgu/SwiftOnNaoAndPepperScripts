43a3a0d9a7136685a1f1eaf0ee29b13c4eb0eb2a
Callum McColl <callum.mccoll1@gmail.com>
2019-11-10 05:17:34 +0000

    tar up the cross toolchain

--- pepper/finalise.sh
+++ pepper/finalise.sh
@@ -6,3 +6,4 @@ tar -czf pepper.tar.gz $INSTALL_PREFIX/lib
 rm -rf $CROSS_TOOLCHAIN_DIR/$TRIPLE/sysroot/home
 mkdir -p $CROSS_TOOLCHAIN_DIR/$TRIPLE/sysroot/home/nao/swift-tc
 cp -r $INSTALL_PREFIX/* $CROSS_TOOLCHAIN_DIR/$TRIPLE/sysroot/home/nao/swift-tc/
+tar -czvf ctc.tar.gz $CROSS_DIR
