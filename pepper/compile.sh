8d4cd5257af187ef73f2e5297e2bff8add8af0d1
Callum McColl <callum.mccoll1@gmail.com>
2019-11-10 04:18:19 +0000

    Made compile.sh create a swift program that talks to c

--- pepper/compile.sh
+++ pepper/compile.sh
@@ -2,6 +2,9 @@
 
 source setup.sh
 
-echo 'print("Hello World!")' > main.swift
+echo 'import Glibc' > main.swift
+echo '' >> main.swift
+echo 'let x = rand()' >> main.swift
+echo 'print("Random: \(x)")' >> main.swift
 swiftc -c main.swift -target i686-aldebaran-linux-gnu
 $CROSS_TOOLCHAIN_DIR/$TRIPLE/bin/ld.gold -pie -z relro --hash-style=gnu --eh-frame-hdr -m elf_i386 -dynamic-linker /lib/ld-linux.so.2 -o main $LFS/usr/lib/Scrt1.o $LFS/usr/lib/crti.o $GCC_TOOLCHAIN/crtbeginS.o -L$INSTALL_PREFIX/lib/swift/linux -L$GCC_TOOLCHAIN -L$INSTALL_PREFIX/lib -L$LFS/usr/lib -L$LFS/lib /usr/local/var/swiftenv/versions/5.1-pepper/usr/lib/swift/linux/i686/swiftrt.o main.o -lswiftSwiftOnoneSupport -lswiftCore -lswiftCore --sysroot=/root/src/nao_swift/pepper/sysroot -lstdc++ -lm -lgcc_s -lgcc -lc -lgcc_s -lgcc $GCC_TOOLCHAIN/crtendS.o $LFS/usr/lib/crtn.o -R/home/nao/swift-tc/lib/swift/linux -R/home/nao/swift-tc/lib
