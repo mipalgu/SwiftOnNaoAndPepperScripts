28e68168338baff6f7970c91ff7a6fafe5bc6ac1
Callum McColl <callum.mccoll1@gmail.com>
2019-11-10 04:30:51 +0000

    Changed compile.sh and compile-swiftenv-tc.sh to allow swift programs to call C functions

--- pepper/compile-swiftenv-tc.sh
+++ pepper/compile-swiftenv-tc.sh
@@ -9,3 +9,5 @@ rm -rf $swiftenv_dir/versions/$version-pepper
 cp -R $swiftenv_dir/versions/$version $swiftenv_dir/versions/$version-pepper
 rm -rf $swiftenv_dir/versions/$version-pepper/usr/lib/swift/*
 cp -R $INSTALL_PREFIX/lib/swift/* $swiftenv_dir/versions/$version-pepper/usr/lib/swift/
+# path the glibc module map so that the paths point to the sysroot.
+sed -i "s|header \"/|header \"$LFS/|g" $swiftenv_dir/versions/$version-pepper/usr/lib/swift/linux/i686/glibc.modulemap
