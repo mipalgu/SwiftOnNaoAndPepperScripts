921dbfc485d8e1354f38022688c800e7284a3316
Callum McColl <callum.mccoll1@gmail.com>
2019-11-29 18:11:48 +1000

    Moved the getopt stuff into setup.sh

--- pepper/build.sh
+++ pepper/build.sh
@@ -2,35 +2,6 @@
 set -e
 
 source setup.sh
-
-usage() { echo "Usage: $0 [-j<value>] -l -s <swift-version> -t <swift-tag>"; }
-
-while getopts "j:hls:t:" o; do
-    case "${o}" in
-        h)
-            usage
-            exit 0
-            ;;
-        j)
-            PARALLEL=${OPTARG}
-            ;;
-        l)
-            BUILD_LIBCXX=true
-	        ;;
-        s)
-	        SWIFT_VERSION=swift-${OPTARG}-RELEASE
-	        ;;
-        t)
-	        SWIFT_VERSION=${OPTARG}
-	        ;;
-        *)
-            echo "Invalid argument ${o}"
-            usage 1>&2
-            exit 1
-            ;;
-    esac
-done
-
 source setup-sources.sh
 source setup-sysroot.sh
 source build-cross-binutils.sh
--- pepper/setup.sh
+++ pepper/setup.sh
@@ -29,4 +29,32 @@ BUILD_DIR=$SRC_DIR/build
 INSTALL_PREFIX=$LFS/home/nao/swift-tc
 export LANG=/usr/lib/locale/en_US
 
+usage() { echo "Usage: $0 [-j<value>] -l -s <swift-version> -t <swift-tag>"; }
+
+while getopts "j:hls:t:" o; do
+    case "${o}" in
+        h)
+            usage
+            exit 0
+            ;;
+        j)
+            PARALLEL=${OPTARG}
+            ;;
+        l)
+            BUILD_LIBCXX=true
+	        ;;
+        s)
+	        SWIFT_VERSION=swift-${OPTARG}-RELEASE
+	        ;;
+        t)
+	        SWIFT_VERSION=${OPTARG}
+	        ;;
+        *)
+            echo "Invalid argument ${o}"
+            usage 1>&2
+            exit 1
+            ;;
+    esac
+done
+
 fi # End SETUP_SH_INCLUDED
