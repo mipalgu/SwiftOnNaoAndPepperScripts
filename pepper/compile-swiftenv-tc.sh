8070f586248b427e606940baf24c3fe439449f2b
Callum McColl <callum.mccoll1@gmail.com>
2020-02-22 15:27:09 +0000

    Changed compile-swiftenv-tc to symlink the sysroot

--- pepper/compile-swiftenv-tc.sh
+++ pepper/compile-swiftenv-tc.sh
@@ -21,8 +21,8 @@ then
     echo "Creating a swiftenv toolchain that cross compiles to the pepper."
     rm -rf $swiftenv_dir/versions/$version-pepper
     cp -R $swiftenv_dir/versions/$version $swiftenv_dir/versions/$version-pepper
-    rm -rf $swiftenv_dir/versions/$version-pepper/usr/lib/swift/*
-    cp -R $INSTALL_PREFIX/lib/swift/* $swiftenv_dir/versions/$version-pepper/usr/lib/swift/
+    rm -rf $swiftenv_dir/versions/$version-pepper/usr/lib/swift
+    ln -s $INSTALL_PREFIX/lib/swift $swiftenv_dir/versions/$version-pepper/usr/lib/
     # path the glibc module map so that the paths point to the sysroot.
     sed -i "s|header \"/|header \"$LFS/|g" $swiftenv_dir/versions/$version-pepper/usr/lib/swift/linux/i686/glibc.modulemap
 fi
