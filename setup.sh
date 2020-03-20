f575d68de1994a1cf477cb0ff049c201af3c6247
Callum McColl <callum.mccoll1@gmail.com>
2020-03-20 15:28:50 +0000

    Moved check to setup.sh and added include guard to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 set +h
 umask 022
 
+if [ -z "$SETUP_SH_INCLUDED" ]
+then
+SETUP_SH_INCLUDED=yes
+
 WD=`pwd`
 SRC_DIR=$WD
 LFS=/tools
@@ -9,3 +13,12 @@ BUILD_DIR=/$WD/build
 
 mkdir -p $SRC_DIR
 mkdir -p $BUILD_DIR
+
+function check() {
+	if [ ! -f $1 ]
+	then
+		$2
+	fi
+}
+
+fi
