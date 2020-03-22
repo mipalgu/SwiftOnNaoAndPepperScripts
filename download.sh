c2cae74d4a5d2a2cabd70f631cfc8406d3801b8e
Callum McColl <callum.mccoll1@gmail.com>
2020-03-22 18:09:03 +0000

    Use check in download

--- download.sh
+++ download.sh
@@ -5,14 +5,19 @@ if [ -z "$DOWNLOAD_SH_INCLUDED" ]
 then
 DOWNLOAD_SH_INCLUDED=yes
 
+source setup.sh
+
 function download() {
 	url=$1
 	[[ -z "$2" ]] && filename=`basename $url` || filename="$2"
 	[[ -z "$3" ]] && outname=$filename || outname="$3"
-	PATH="/usr/local/bin:$PATH" LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH" wget $url
-	if [ $filename != $outname ]
-	then
-		mv $filename $outname
-	fi
+	function _download() {
+		PATH="/usr/local/bin:$PATH" LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH" wget $url
+		if [ $filename != $outname ]
+		then
+			mv $filename $outname
+		fi
+	}
+	check _download
 }
 fi
