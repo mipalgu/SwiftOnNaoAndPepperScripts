91a5463823259fb108b995b68a6515ef15cf5c71
Callum McColl <callum.mccoll1@gmail.com>
2020-03-22 18:36:07 +0000

    Changed to  in download-tools

--- download-tools.sh
+++ download-tools.sh
@@ -9,3 +9,4 @@ cd $SRC_DIR
 download https://cmake.org/files/v$CMAKE_MAJOR_VERSION.$CMAKE_MINOR_VERSION/cmake-$CMAKE_VERSION.tar.gz
 download http://github.com/ninja-build/ninja/archive/v$NINJA_VERSION.tar.gz ninja-$NINJA_VERSION.tar.gz
 
+cd $WD
