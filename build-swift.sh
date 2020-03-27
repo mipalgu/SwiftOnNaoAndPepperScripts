09cba8b12c13ec0e0f6d71e0263b010f0e62104c
Callum <callum.mccoll1@gmail.com>
2020-03-27 02:08:25 +0000

    Successfully built swift

--- build-swift.sh
+++ build-swift.sh
@@ -29,12 +29,12 @@ function swift() {
       -DCMAKE_ASM_COMPILER="$HOST_CLANG" \
       -DPYTHON_EXECUTABLE="${PYTHON}" \
       -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
-      -DSWIFT_PATH_TO_CMARK_SOURCE="$SRC_DIR/cmark" \
+      -DSWIFT_PATH_TO_CMARK_SOURCE="$SRC_DIR/apple/cmark" \
       -DSWIFT_PATH_TO_CMARK_BUILD="$CMARK_BUILD_DIR" \
       -DSWIFT_CMARK_LIBRARY_DIR="$CMARK_BUILD_DIR/src" \
-      -DSWIFT_PATH_TO_LLVM_SOURCE="$SRC_DIR/llvm" \
+      -DSWIFT_PATH_TO_LLVM_SOURCE="$SRC_DIR/apple/llvm" \
       -DSWIFT_PATH_TO_LLVM_BUILD="$LLVM_BUILD_DIR" \
-      -DSWIFT_PATH_TO_CLANG_SOURCE="$SRC_DIR/clang" \
+      -DSWIFT_PATH_TO_CLANG_SOURCE="$SRC_DIR/apple/clang" \
       -DSWIFT_PATH_TO_CLANG_BUILD="$LLVM_BUILD_DIR" \
       -DSWIFT_INCLUDE_DOCS=FALSE \
       -DSWIFT_INCLUDE_TESTS=TRUE \
@@ -42,7 +42,7 @@ function swift() {
       -DSWIFT_BUILD_DYNAMIC_SDK_OVERLAY=TRUE \
       -DSWIFT_BUILD_RUNTIME_WITH_HOST_COMPILER=TRUE \
       -DSWIFT_STDLIB_BUILD_TYPE=$CMAKE_BUILD_TYPE \
-      -DSWIFT_SOURCE_DIR="$SRC_DIR/swift" \
+      -DSWIFT_SOURCE_DIR="$SRC_DIR/apple/swift" \
       -DSWIFT_HOST_VARIANT="linux" \
       -DSWIFT_HOST_VARIANT_SDK="LINUX" \
       -DSWIFT_HOST_VARIANT_ARCH="$ARCH" \
@@ -51,10 +51,10 @@ function swift() {
       -DSWIFT_PRIMARY_VARIANT_SDK="LINUX" \
       -DSWIFT_PRIMARY_VARIANT_ARCH="$ARCH" \
       -DSWIFT_PRIMARY_VARIANT_TRIPLE="$TRIPLE" \
-      -DCMAKE_C_FLAGS="-gcc-toolchain $CROSS_DIR -Wno-c++11-narrowing -target ${TRIPLE} $INCLUDE_FLAGS -fno-use-cxa-atexit -fPIC $BINARY_FLAGS -I$CROSS_DIR/$TRIPLE/include/c++/$GCC_VERSION -I$CROSS_DIR/$TRIPLE/include/c++/$GCC_VERSION/$TRIPLE" \
-      -DCMAKE_CXX_FLAGS="-gcc-toolchain $CROSS_DIR -Wno-c++11-narrowing -target ${TRIPLE} $INCLUDE_FLAGS -fno-use-cxa-atexit -fPIC $BINARY_FLAGS -I$CROSS_DIR/$TRIPLE/include/c++/$GCC_VERSION -I$CROSS_DIR/$TRIPLE/include/c++/$GCC_VERSION/$TRIPLE" \
-      -DCMAKE_EXE_LINKER_FLAGS="$LINK_FLAGS -gcc-toolchain $CROSS_DIR -fno-use-cxa-atexit -luuid -lpthread -fvisibility=protected -Bsymbolic" \
-      -DCMAKE_SHARED_LINKER_FLAGS="$LINK_FLAGS -gcc-toolchain $CROSS_DIR -fno-use-cxa-atexit -luuid -lpthread -fvisibility=protected -Bsymbolic" \
+      -DCMAKE_C_FLAGS="-gcc-toolchain $LFS -Wno-c++11-narrowing -target ${TRIPLE} $INCLUDE_FLAGS -fno-use-cxa-atexit -fPIC $BINARY_FLAGS -I$LFS/$TRIPLE/include/c++/$GCC_VERSION -I$LFS/$TRIPLE/include/c++/$GCC_VERSION/$TRIPLE" \
+      -DCMAKE_CXX_FLAGS="-gcc-toolchain $LFS -Wno-c++11-narrowing -target ${TRIPLE} $INCLUDE_FLAGS -fno-use-cxa-atexit -fPIC $BINARY_FLAGS -I$LFS/$TRIPLE/include/c++/$GCC_VERSION -I$LFS/$TRIPLE/include/c++/$GCC_VERSION/$TRIPLE" \
+      -DCMAKE_EXE_LINKER_FLAGS="$LINK_FLAGS -gcc-toolchain $LFS -fuse-ld="$CROSS_DIR/bin/$TRIPLE-ld.gold" -fno-use-cxa-atexit -luuid -lpthread -fvisibility=protected -Bsymbolic" \
+      -DCMAKE_SHARED_LINKER_FLAGS="$LINK_FLAGS -gcc-toolchain $LFS -fuse-ld="$CROSS_DIR/bin/$TRIPLE-ld.gold" -fno-use-cxa-atexit -luuid -lpthread -fvisibility=protected -Bsymbolic" \
       -DSWIFT_ENABLE_GOLD_LINKER=TRUE \
       -DSWIFT_ENABLE_LLD_LINKER=FALSE \
       -DSWIFT_NATIVE_SWIFT_TOOLS_PATH="/usr/local/var/swiftenv/shims" \
@@ -65,12 +65,12 @@ function swift() {
       -DSWIFT_LINUX_${ARCH}_ICU_UC_INCLUDE="$INSTALL_PREFIX/include" \
       -DSWIFT_LINUX_${ARCH}_ICU_I18N="$INSTALL_PREFIX/lib/libicui18n.so" \
       -DSWIFT_LINUX_${ARCH}_ICU_I18N_INCLUDE="$INSTALL_PREFIX/include" \
-      -DLIBXML2_INCLUDE_DIR="$CROSS_DIR/xml2/include/libxml2" \
-      -DLIBXML2_LIBRARY="$CROSS_DIR/xml2/lib/libxml2.so" \
+      -DLIBXML2_INCLUDE_DIR="$LFS/include/libxml2" \
+      -DLIBXML2_LIBRARY="$LFS/lib/libxml2.so" \
       -DLLVM_DIR="$LLVM_BUILD_DIR/lib/cmake/llvm" \
       -DClang_DIR="$LLVM_BUILD_DIR/lib/cmake/clang" \
       -DSWIFT_INCLUDE_TOOLS=FALSE \
-      $SRC_DIR/swift
+      $SRC_DIR/apple/swift
     cd $WD
     cmake --build $SWIFT_BUILD_DIR -- -j${PARALLEL}
     touch $SWIFT_BUILD_DIR/lib/swift/linux/i686/StdlibUnicodeUnittest.swiftmodule
