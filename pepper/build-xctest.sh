8a1e7862a4228d891408a10332ed16955c0fe93f
Callum McColl <callum.mccoll1@gmail.com>
2020-04-10 04:22:27 +0000

    Updated build-xctest.sh so that it build xctest for swift 5.2

--- pepper/build-xctest.sh
+++ pepper/build-xctest.sh
@@ -5,8 +5,8 @@ source build-config.sh
 source cross.sh
 source compile-swiftenv-tc.sh
 
-swift_include_flags=`echo "$INCLUDE_FLAGS -I$INSTALL_PREFIX/lib/swift -I$INSTALL_PREFIX/lib/swift/clang/include" | sed 's/ /;/g'`
-swift_link_flags=`echo "$LINK_FLAGS" | sed 's/^/-Xlinker;/g' | sed 's/  / /g' | sed 's/ /;-Xlinker;/g'`
+swift_include_flags=`echo "$INCLUDE_FLAGS -I$INSTALL_PREFIX/lib/swift -I$INSTALL_PREFIX/lib/swift/clang/include" | sed 's/ /;/g' | sed 's/;/ /g'`
+swift_link_flags=`echo "$LINK_FLAGS" | sed 's/^/-Xlinker;/g' | sed 's/  / /g' | sed 's/ /;-Xlinker;/g' | sed 's/;/ /g'`
 swift_link_flags=
 
 if [ ! -f $XCTEST_BUILD_DIR/.xctest-build-cross ]
@@ -15,6 +15,11 @@ then
     /usr/local/var/swiftenv/bin/swiftenv local $swiftenv_swift_version-pepper
     rm -rf $XCTEST_BUILD_DIR
     mkdir -p $XCTEST_BUILD_DIR
+    if [ ! -f $SRC_DIR/swift-corelibs-libdispatch/dispatch/module.modulemap.orig ]
+    then
+	    cp $SRC_DIR/swift-corelibs-libdispatch/dispatch/module.modulemap $SRC_DIR/swift-corelibs-libdispatch/dispatch/module.modulemap.orig
+    fi
+    rm -f $SRC_DIR/swift-corelibs-libdispatch/dispatch/module.modulemap
     cd $XCTEST_BUILD_DIR
     PATH="$CROSS_DIR/bin:$PATH" CC="$HOST_CLANG" CXX="$HOST_CLANGXX" CPATH="$CPATH" LIBRARY_PATH="$LIBRARY_PATH" LD="$CROSS_DIR/bin/$TRIPLE-ld.gold" cmake -G "Ninja" \
 	    -DCMAKE_CROSSCOMPILING=TRUE \
@@ -30,10 +35,12 @@ then
             -DCMAKE_C_FLAGS="-gcc-toolchain $CROSS_DIR -fno-stack-protector $INCLUDE_FLAGS $BINARY_FLAGS -I$INSTALL_PREFIX/lib/swift/Block -I$INSTALL_PREFIX/lib/swift" \
             -DCMAKE_EXE_LINKER_FLAGS="-gcc-toolchain $CROSS_DIR $LINK_FLAGS" \
             -DCMAKE_SHARED_LINKER_FLAGS="-gcc-toolchain $CROSS_DIR $LINK_FLAGS" \
-	    -DCMAKE_SWIFT_COMPILER="/usr/local/var/swiftenv/shims/swiftc" \
-	    -DCMAKE_SWIFT_FLAGS="-I$INSTALL_PREFIX/lib/swift/linux/i686;$swift_include_flags;-I$LFS/usr/include;-I$LFS/include;-I$CROSS_DIR/lib/gcc/$TRIPLE/$GCC_VERSION/include-fixed;-sdk;$LFS;$swift_link_flags;" \
+	    -Ddispatch_DIR="$DISPATCH_BUILD_DIR/cmake/modules" \
+	    -DFoundation_DIR="$FOUNDATION_BUILD_DIR/cmake/modules" \
+	    -DCMAKE_Swift_COMPILER="/usr/local/var/swiftenv/shims/swiftc" \
+	    -DCMAKE_Swift_FLAGS="-I$INSTALL_PREFIX/lib/swift/linux/i686 $swift_include_flags -I$LFS/usr/include -I$LFS/include -I$CROSS_DIR/lib/gcc/$TRIPLE/$GCC_VERSION/include-fixed -sdk $LFS $swift_link_flags -target $TRIPLE" \
 	    -DAST_TARGET="$TRIPLE" \
-	    -DCMAKE_SWIFT_LINK_FLAGS="-L$INSTALL_PREFIX/swift/linux;-L$INSTALL_PREFIX/lib/swift/linux;-L$INSTALL_PREFIX/lib;-L$LFS/usr/lib;-L$LFS/lib;-sdk;$LFS;-target;$TRIPLE" \
+	    -DCMAKE_Swift_LINK_FLAGS="-L$INSTALL_PREFIX/swift/linux -L$INSTALL_PREFIX/lib/swift/linux -L$INSTALL_PREFIX/lib -L$LFS/usr/lib -L$LFS/lib -sdk $LFS -target $TRIPLE" \
             -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE \
 	    -DBUILD_SHARED_LIBS=YES \
 	    -DCMAKE_SYSTEM_PROCESSOR=$ARCH \
